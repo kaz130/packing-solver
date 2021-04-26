dec6918341b36131e64e89aec0e861658f56cbf6
Kazuki Aizawa <4954175+kaz130@users.noreply.github.com>
2021-04-26 22:25:11 +0900

    Add Box

---
+++ packsolver/box.py
@@ -0,0 +1,10 @@
+from typing import Set, Tuple
+
+
+class Box:
+    def __init__(self, width: int, height: int) -> None:
+        self.width = width
+        self.height = height
+
+    def get_all_placements(self) -> Set[Tuple[int, int]]:
+        return set([(self.width, self.height), (self.height, self.width)])

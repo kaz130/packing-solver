48c504334cca1c6a1589471af78ddb9d65a1321c
Kazuki Aizawa <4954175+kaz130@users.noreply.github.com>
2021-04-26 22:24:48 +0900

    Add tests

---
+++ tests/test_box.py
@@ -0,0 +1,11 @@
+from packsolver.box import Box
+
+
+def test_boxは幅と高さを持つ():
+    box = Box(3, 4)
+    assert (box.width, box.height) == (3, 4)
+
+
+def test_boxの全ての置き方を取得する():
+    box = Box(3, 4)
+    assert box.get_all_placements() == set([(3, 4), (4, 3)])

60073e4805bc1561a77c03cbdda8e55208a96f54
Kazuki Aizawa <4954175+kaz130@users.noreply.github.com>
2021-04-27 22:53:28 +0900

    Add init

--- packsolver/packsolver.py
+++ packsolver/packsolver.py
@@ -1,4 +1,7 @@
+import os
+from dotenv import load_dotenv
 import toml
+from amplify.client import FixstarsClient
 
 from packsolver.box import Box
 from packsolver.container import Container
@@ -6,7 +9,12 @@ from packsolver.container import Container
 
 class PackSolver:
     def __init__(self) -> None:
-        pass
+        load_dotenv()
+        self.client = FixstarsClient()
+        self.client.token = os.getenv("TOKEN")
+        self.client.parameters.timeout = 10000
+        if os.getenv("https_proxy") is not None:
+            self.client.proxy = os.getenv("https_proxy")
 
     def load(self, problem_file: str) -> None:
         with open(problem_file) as f:

c9e9c6aca96dc8b7fbbb704ddb95fcd42410a117
Kazuki Aizawa <4954175+kaz130@users.noreply.github.com>
2021-04-28 22:59:25 +0900

    Add constraint test

--- tests/test_packsolver.py
+++ tests/test_packsolver.py
@@ -1,8 +1,22 @@
+import pytest
 from packsolver.packsolver import PackSolver
 
 
-def test_入力ファイルを読み込む():
+@pytest.fixture
+def solver():
     solver = PackSolver()
     solver.load("tests/sample1.toml")
-    assert [(b.width, b.height) for b in solver.boxes] == [(3, 4), (12, 4), (6, 8)]
-    assert (solver.container.width, solver.container.height) == (10, 12)
+    return solver
+
+
+def test_入力ファイルを読み込む(solver):
+    assert [(b.width, b.height) for b in solver.boxes] == [(1, 3), (6, 2)]
+    assert (solver.container.width, solver.container.height) == (4, 5)
+
+
+def test_containerの中に入らない置き方を除外する(solver):
+    q = solver.prepare_symbols()
+    assert not q[1][3][0][0].is_number()
+    assert not q[1][3][0][0].is_number()
+    assert q[1][4][0][0] == 0
+    assert q[2][3][0][1] == 0

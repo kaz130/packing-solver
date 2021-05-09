53b8a8e0c4c13a89d762f58addab3d2e5d8d7bdb
Kazuki Aizawa <4954175+kaz130@users.noreply.github.com>
2021-05-09 23:03:24 +0900

    Add board constraints

--- packsolver/packsolver.py
+++ packsolver/packsolver.py
@@ -1,13 +1,15 @@
 import os
 from itertools import product
-from typing import List
+from typing import List, Tuple
 from dotenv import load_dotenv
 import toml
 from amplify import (
     BinaryPoly,
+    BinaryConstraint,
     gen_symbols,
 )
 from amplify.client import FixstarsClient
+from amplify.constraint import less_equal
 
 from packsolver.box import Box
 from packsolver.container import Container
@@ -30,18 +32,29 @@ class PackSolver:
         self.container = Container(problem["container"][0], problem["container"][1])
 
     def prepare_symbols(self) -> List[List[List[List[BinaryPoly]]]]:
-        q = gen_symbols(
-            BinaryPoly, self.container.width, self.container.height, len(self.boxes), 2
-        )
+        q = gen_symbols(BinaryPoly, self.container.width, self.container.height, len(self.boxes), 2)
         for x, y in product(range(self.container.width), range(self.container.height)):
             for i, b in enumerate(self.boxes):
                 for j, p in enumerate(b.all_placements):
-                    if (
-                        x + p[0] > self.container.width
-                        or y + p[1] > self.container.height
-                    ):
+                    if x + p[0] > self.container.width or y + p[1] > self.container.height:
                         q[x][y][i][j] = BinaryPoly(0)
                 for j in range(len(b.all_placements), 2):
                     q[x][y][i][j] = BinaryPoly(0)
 
         return q
+
+    def make_board_constraints(self, q: List) -> List[BinaryConstraint]:
+        s = dict()
+        for p in product(range(self.container.width), range(self.container.height)):
+            s[p] = BinaryPoly()
+
+        for i, box in enumerate(self.boxes):
+            for j, p in enumerate(box.all_placements):
+                for bx, by in product(range(p[0]), range(p[1])):
+                    for cx, cy in product(
+                        range(self.container.width - p[0] + 1),
+                        range(self.container.height - p[1] + 1),
+                    ):
+                        s[(cx + bx, cy + by)] += q[cx][cy][i][j]
+        board_constraints = [less_equal(q, 1) for q in s.values()]
+        return board_constraints
--- tests/test_packsolver.py
+++ tests/test_packsolver.py
@@ -1,3 +1,4 @@
+from itertools import chain
 import pytest
 from packsolver.packsolver import PackSolver
 
@@ -30,13 +31,15 @@ def test_containerの中に入らない置き方を除外する(solver):
 
 def test_box同士は重ならないよう制約条件を設定する(solver):
     q = solver.prepare_symbols()
-    c = solver.make_board_constraints(q)
+    constaraints = solver.make_board_constraints(q)
     test_case = [[[[0, 0] for k in range(2)] for j in range(5)] for i in range(4)]
     test_case[0][1][0][0] = 1
     test_case[2][3][0][0] = 1
-    assert c.is_satisfied(test_case) is True
+    test_case = list(chain.from_iterable(chain.from_iterable(chain.from_iterable(test_case))))
+    assert all([c.is_satisfied(test_case) for c in constaraints])
 
     test_case = [[[[0, 0] for k in range(2)] for j in range(5)] for i in range(4)]
-    test_case[2][2][0][0] = 1
-    test_case[2][2][0][0] = 1
-    assert c.is_satisfied(test_case) is False
+    test_case[1][2][0][0] = 1
+    test_case[1][2][1][0] = 1
+    test_case = list(chain.from_iterable(chain.from_iterable(chain.from_iterable(test_case))))
+    assert any([c.is_satisfied(test_case) is False for c in constaraints]) is True

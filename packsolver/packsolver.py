cda0cf121adcf7cb7d839b1e61d45d73452bf136
Kazuki Aizawa <4954175+kaz130@users.noreply.github.com>
2021-05-11 23:53:17 +0900

    Add solver

--- packsolver/packsolver.py
+++ packsolver/packsolver.py
@@ -5,8 +5,11 @@ from dotenv import load_dotenv
 import toml
 from amplify import (
     BinaryPoly,
+    BinaryQuadraticModel,
     BinaryConstraint,
     gen_symbols,
+    Solver,
+    decode_solution,
 )
 from amplify.client import FixstarsClient
 from amplify.constraint import less_equal, equal_to
@@ -20,7 +23,7 @@ class PackSolver:
         load_dotenv()
         self.client = FixstarsClient()
         self.client.token = os.getenv("TOKEN")
-        self.client.parameters.timeout = 10000
+        self.client.parameters.timeout = 1000
         if os.getenv("https_proxy") is not None:
             self.client.proxy = os.getenv("https_proxy")
 
@@ -31,6 +34,25 @@ class PackSolver:
         self.boxes = [Box(b[0], b[1]) for b in problem["boxes"]]
         self.container = Container(problem["container"][0], problem["container"][1])
 
+    def solve(self) -> List:
+        solver = Solver(self.client)
+        q = self.prepare_symbols()
+        constraints = sum(self.make_board_constraints(q)) + sum(self.make_once_constraints(q))
+        model = BinaryQuadraticModel(constraints)
+        result = solver.solve(model)
+        if len(result) == 0:
+            raise RuntimeError("Any one of constaraints is not satisfied.")
+        solution = result[0]
+        values = solution.values
+        q_values = decode_solution(q, values)
+        ret = list()
+        for x, y in product(range(self.container.width), range(self.container.height)):
+            for i, b in enumerate(self.boxes):
+                for j, p in enumerate(b.all_placements):
+                    if q_values[x][y][i][j] == 1:
+                        ret.append((x, y, p[0], p[1]))
+        return ret
+
     def prepare_symbols(self) -> List[List[List[List[BinaryPoly]]]]:
         q = gen_symbols(BinaryPoly, self.container.width, self.container.height, len(self.boxes), 2)
         for x, y in product(range(self.container.width), range(self.container.height)):
@@ -40,7 +62,6 @@ class PackSolver:
                         q[x][y][i][j] = BinaryPoly(0)
                 for j in range(len(b.all_placements), 2):
                     q[x][y][i][j] = BinaryPoly(0)
-
         return q
 
     def make_board_constraints(self, q: List) -> List[BinaryConstraint]:
--- tests/test_packsolver.py
+++ tests/test_packsolver.py
@@ -10,6 +10,16 @@ def solver():
     return solver
 
 
+@pytest.mark.skip(reason="APIの呼び出しを含むテスト")
+def test_boxの配置を最適化する(solver):
+    p = [[False] * 5 for _ in range(4)]
+    for x, y, w, h in solver.solve():
+        for bx in range(x, x + w):
+            for by in range(y, y + h):
+                assert p[bx][by] is False
+                p[bx][by] = True
+
+
 def test_入力ファイルを読み込む(solver):
     assert [(b.width, b.height) for b in solver.boxes] == [(1, 3), (2, 2)]
     assert (solver.container.width, solver.container.height) == (4, 5)

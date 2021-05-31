ea65a00c7123b06979652df667862b4555ff077e
Kazuki Aizawa <4954175+kaz130@users.noreply.github.com>
2021-05-31 23:39:52 +0900

    Add rotating option

--- packsolver/box.py
+++ packsolver/box.py
@@ -3,14 +3,21 @@ from typing import Iterable, Optional
 
 
 class Box:
-    def __init__(self, width: int, height: int, depth: Optional[int] = None) -> None:
+    def __init__(self, width: int, height: int, depth: Optional[int] = None, can_rotate: bool = True) -> None:
         self.width = width
         self.height = height
         self.depth = depth
+        self.can_rotate = can_rotate
 
     @property
     def all_placements(self) -> Iterable:
-        if self.depth is None:
-            return sorted(set(permutations([self.width, self.height], 2)))
+        if self.can_rotate:
+            if self.depth is None:
+                return sorted(set(permutations([self.width, self.height], 2)))
+            else:
+                return sorted(set(permutations([self.width, self.height, self.depth], 3)))
         else:
-            return sorted(set(permutations([self.width, self.height, self.depth], 3)))
+            if self.depth is None:
+                return [(self.width, self.height)]
+            else:
+                return [(self.width, self.height, self.depth)]
